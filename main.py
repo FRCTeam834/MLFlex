38654a0f8d72cda2ba0c6a5024e583d37813e8a0
CAP1Sup <42127153+CAP1Sup@users.noreply.github.com>
2020-07-04 20:45:00 -0400

    Fixed naming issue

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ input_folder = os.path.abspath("./testing/input/Dual Balls Video/")
 output_folder = os.path.abspath("./testing/output")
 
 # Parameters
-supported_formats = '.jpg', '.JPG', '.png', '.PNG' # .............................. Specifies the allowable image formats
+
 
 def main():
     convert_Supervisely_2_Pascal_VOC(input_folder, output_folder, False)
