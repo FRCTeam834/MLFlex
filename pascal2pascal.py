4979c9e886d969d8bb9b6d45b19d8a35e2e5276d
Christian Piper <xmanpiper@gmail.com>
2020-08-21 13:23:42 -0400

    Pascal2pascal should be finished

--- pascal2pascal.py
+++ pascal2pascal.py
@@ -2,15 +2,16 @@
 import xml.etree.ElementTree as xml
 from xml.etree import ElementTree
 from xml.dom import minidom
+from PIL import Image
 import argparse
-import glob
+import shutil
 import os
 
 # Setup constants for later use
 whitelist =    ['red_robot', 'blue_robot', 'red_power_port_high_goal', 'blue_power_port_high_goal', 'power_cell', 'color_wheel']
 convert_list = ['Red_Robot', 'Blue_Robot', 'Goal',                     'Goal',                      'Power_Cell', 'Color_Wheel']
 
-'''
+
 def main():
 
     # Initialize parser 
@@ -20,46 +21,137 @@ def main():
     required_args = parser.add_argument_group('required arguments')
 
     # Add arguments
-    # Required (String) arguments
-    required_args.add_argument("-i", "--input",  required = True, help = "Input path to the folder containing the Supervisely annotations.")
-    required_args.add_argument("-o", "--output", required = True, help = "Output path for the .zip file")
+    # String arguments
+    required_args.add_argument("-i", "--input",  required = True, help = "Input path to the folder containing the data to be converted.")
+    parser.add_argument("-o", "--output", required = True, help = "Output path for the converted data. All images will be copied here as well. If nothing is specified, then the .xmls in the input directory will be modifed.")
 
     # Optional (Boolean) arguments
-    parser.add_argument("-j", "--join",         action = "store_true", default = False, help = "If parameter is specified, then if multiple Supervisely projects exist in the same folder, they will be joined into a single .zip instead of individual .zip files")
-    parser.add_argument("-c", "--cleanup",      action = "store_true", default = False, help = "If parameter is specified, then the input Supervisely path will be automatically deleted after use")
-    parser.add_argument("-d", "--debug",        action = "store_true", default = False, help = "If parameter is specified, then the temporary folders will be left for examination")
+    parser.add_argument("-c", "--cleanup",      action = "store_true", default = False, help = "If parameter is specified, then the input folder path will be automatically deleted after use")
     parser.add_argument("-f", "--feedback",     action = "store_true", default = True,  help = "If parameter is specified, then the feedback will be provided during the conversion process")
-    parser.add_argument("-a", "--augmentation", action = "store_true", default = True,  help = "If parameter is specified, then additional flipped and cropped versions of the images and annotations will be included in the output")
     
     # Read arguments from command line 
     args = parser.parse_args()
     
-    # Get the full paths
+    # Get the full input path
     input_path = os.path.abspath(args.input)
-    output_path = os.path.abspath(args.output)
 
-    # Find out what Supervisely folders are available
-    subfolders = [ f.path for f in os.scandir(input_path) if f.is_dir() ]
+    # If we have an output path
+    if args.output is not None:
 
-    # Now run through with the valid folders
-    current_folder_index = 0
-    for folder in subfolders:
-        # Print feedback
-        if args.feedback:
-            print()
-            print("Converting " + os.path.basename(folder))
+        # Get the full output path
+        output_path = os.path.abspath(args.output)
 
+        # Create the output folder if necessary
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+            
+            # Provide feedback
+            if args.feedback:
+                print("Created output folder")
 
-        # Convert
-        if args.join:
-            convert_Supervisely_2_Pascal_VOC(os.path.abspath(folder), output_path, args.cleanup, args.feedback, args.augmentation, args.augmentation, args.augmentation, last_folder, debug = args.debug)
         else:
-            convert_Supervisely_2_Pascal_VOC(os.path.abspath(folder), output_path, args.cleanup, args.feedback, args.augmentation, args.augmentation, args.augmentation, True, debug = args.debug)
+            # Provide feedback
+            if args.feedback:
+                print("Output folder exists, no need to create it")
 
-        # Increment the counter
-        current_folder_index = current_folder_index + 1
-'''
+        # Read the input directory's files
+        input_files = os.listdir(input_path)
 
+        # Provide feedback
+        if args.feedback:
+            print("Copying files")
+
+        # Create an accumulator
+        current_file_index = 0
+
+        # Copy them over
+        for filename in input_files:
+
+            # Get the file's extension
+            raw_filename, file_ext = os.path.splitext(filename)
+
+            # Copy the .xmls, then convert them
+            if file_ext == ".xml":
+                shutil.copy(os.path.join(input_path, filename), output_path)
+
+                # Convert the copied .xml
+                convert_xml_annotation(filename, output_path, whitelist, convert_list)
+
+            # Copy the .jpgs
+            elif file_ext == ".jpg":
+                shutil.copy(os.path.join(input_path, filename), output_path)
+
+            # Copy the other files, such as photos
+            elif file_ext == ".png":
+
+                # Open the image
+                image = Image.open(os.path.join(input_path, filename))
+
+                # Create the new filename
+                new_filename = raw_filename + '.jpg'
+
+                # Save the image
+                image.save(os.path.join(output_path, new_filename))
+
+            # Provide feedback
+            if args.feedback:
+                print("Current file index: " + str(current_file_index) + "//" + str(len(input_files)))
+
+            # Increment the counter
+            current_file_index = current_file_index + 1
+
+        # Clean up the folder if specified
+        if args.cleanup:
+            shutil.rmtree(input_path)
+
+    # Just modify the input folder's .xmls
+    else:
+        
+        # Get the list of files in the input directory
+        input_files = os.listdir(input_path)
+
+        # Provide feedback
+        if args.feedback:
+            print("Modifying files")
+
+        # Create an accumulator
+        current_file_index = 0
+
+        # Copy them over
+        for filename in input_files:
+
+            # Get the file's extension
+            raw_filename, file_ext = os.path.splitext(filename)
+
+            # Convert the .xml file 
+            if file_ext == ".xml":
+                convert_xml_annotation(filename, output_path, whitelist, convert_list)
+
+            # Copy the other files, such as photos
+            elif file_ext == ".png":
+
+                # Open the image
+                image = Image.open(os.path.join(input_path, filename))
+
+                # Create the new filename
+                new_filename = raw_filename + '.jpg'
+
+                # Save the image
+                image.save(os.path.join(input_path, new_filename))
+
+                # Remove the .png
+                os.remove(os.path.join(input_path, filename))
+
+            # Provide feedback
+            if args.feedback:
+                print("Current file index: " + str(current_file_index) + "/" + str(len(input_files)))
+
+            # Increment the counter
+            current_file_index = current_file_index + 1
+
+
+
+# Function for converting the .xmls
 def convert_xml_annotation(filename, filepath, whitelist, convert_list):
 
     # Accumulator for later deletions
@@ -118,4 +210,4 @@ def convert_xml_annotation(filename, filepath, whitelist, convert_list):
     tree.write(os.path.join(filepath, filename))
 
 
-convert_xml_annotation('example_xml.xml', os.path.abspath('C:\\Users\\Staack\\Desktop\\Christian\\Robotics'), whitelist, convert_list)
+main()
